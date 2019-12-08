62b8fff81aee08fc7bd1596c1df637f9ff16f5fe
Ross Mckay <ross.mckay1975@gmail.com>
2019-12-08 16:48:26 +0000

    add component container grid

---
+++ src/component/Brand.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"; 
+
+class Brand extends Component { 
+
+    render() {
+        return (
+                
+                <div className="grid-container">
+                        <div className="grid-item 1"> 1</div>  
+                        <div className="grid-item 2"> 2</div>
+                        <div className="grid-item 3"> 3</div>
+                        <div className="grid-item 4"> 4</div>
+                        <div className="grid-item 5"> 5</div>
+                        <div className="grid-item 6"> 6</div>  
+                </div>
+
+        )
+
+    }
+
+}
+
+export default Brand
---
+++ src/container/BrandContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+import Brand from '../component/Brand';
+
+class BrandContainer extends Component { 
+
+    render(){ 
+        return( 
+            // <div> I am the container </div>
+            < Brand /> 
+        )
+    }
+}
+
+export default BrandContainer
